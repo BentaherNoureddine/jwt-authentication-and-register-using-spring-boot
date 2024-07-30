e63f6c476279c23a031fef4ea11bfc76c85f4382
Nourdine <noureddinebentaher20@gmail.com>
2024-07-30 21:44:24 +0100

    initial commit

---
+++ forest_api/src/main/java/com/noureddine/forest/ForestApiApplication.java
@@ -0,0 +1,13 @@
+package com.noureddine.forest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForestApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ForestApiApplication.class, args);
+    }
+
+}
---
+++ forest_api/src/test/java/com/noureddine/forest/ForestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.noureddine.forest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForestApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
