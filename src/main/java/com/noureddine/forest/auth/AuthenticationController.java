918a7113ecb098659981fa15f627e00ff00840ce
Nourdine <noureddinebentaher20@gmail.com>
2024-07-31 05:05:39 +0100

    added registration feature

--- src/main/java/com/noureddine/forest/auth/AuthenticationController.java
+++ src/main/java/com/noureddine/forest/auth/AuthenticationController.java
@@ -2,9 +2,12 @@ package com.noureddine.forest.auth;
 
 
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.mail.MessagingException;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("auth")
@@ -13,4 +16,14 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthenticationController {
 
     private final AuthenticationService authenticationService;
+
+    @PostMapping("/register")
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    public ResponseEntity<?> register(
+                    @RequestBody @Valid final RegistrationRequest request
+
+            ) throws MessagingException {
+                 authenticationService.register(request);
+                 return ResponseEntity.accepted().build();
+    }
 }
